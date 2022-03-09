3a7787f7f9e3b79ed4e84a90af45ae78a9c7407d
Elodie <91661119+eloLG@users.noreply.github.com>
2022-03-09 17:37:26 +0100

    Update StringUtils.java

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,43 @@
 package fr.istic.vv;
+import java.util.Stack;
 
 public class StringUtils {
 
     private StringUtils() {}
 
     public static boolean isBalanced(String str) {
-        return false;
+        Stack<Character> pile = new Stack<>();
+		for (Character character: str.toCharArray()) {
+			switch (character) {
+				case '{':
+					pile.add(character);
+					break;
+				case '(':
+					pile.add(character);
+					break;
+				case '[':
+					pile.add(character);
+					break;
+				case '}':
+					if (pile.isEmpty() || pile.pop() != '{'){
+						return false;
+					}
+					break;
+				case ')':
+					if (pile.isEmpty() || pile.pop() != '(') {
+						return false;
+					}
+					break;
+				case ']':
+					if (pile.isEmpty() || pile.pop() != '[') {
+						return false;
+					}
+					break;
+				default:
+					break;
+			}
+		}
+		return pile.isEmpty();
     }
 
 }
