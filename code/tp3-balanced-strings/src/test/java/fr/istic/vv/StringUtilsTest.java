6b22cb990d0a4ae04d1da60a03ee10c9a899a347
Elodie <91661119+eloLG@users.noreply.github.com>
2022-03-09 17:38:16 +0100

    Update StringUtilsTest.java

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -7,5 +7,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+  @Test
+	public void testCorrectSimple(){
+		String test01 = "{[][]}({})";
+		String test02 = "{}";
+		String test03 = "()";
+		String test04 = "[]";
+		String test05 = "{[()]}";
+
+		assertEquals(true, Balance.isBalanced(test01));
+		assertEquals(true, Balance.isBalanced(test02));
+		assertEquals(true, Balance.isBalanced(test03));
+		assertEquals(true, Balance.isBalanced(test04));
+		assertEquals(true, Balance.isBalanced(test05));
+	}
+
+	@Test
+	public void testIncorrect(){
+		String test01 = "][";
+		String test02 = "([)]";
+		String test03 = "{";
+		String test04 = "{(}{}";
+
+		assertEquals(false, Balance.isBalanced(test01));
+		assertEquals(false, Balance.isBalanced(test02));
+		assertEquals(false, Balance.isBalanced(test03));
+		assertEquals(false, Balance.isBalanced(test04));
+	}
+
+	@Test
+	public void testComplex(){
+		String test01 = "{[][aaa]}({bbb})";
+		String test02 = "";
+		String test03 = "abc";
+		assertEquals(true, Balance.isBalanced(test01));
+		assertEquals(true, Balance.isBalanced(test02));
+		assertEquals(true, Balance.isBalanced(test03));
+
+	}
 
 }
